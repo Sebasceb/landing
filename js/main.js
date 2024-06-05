b183a7f0b50090fc897925cabe4683b4b4d08e06
your name <mcpoveda@espol.edu.ec>
2024-06-05 11:00:44 -0500

    Guia 08

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
 let loaded = ( eventLoaded ) => {
+
+    document.getElementById("formulario").addEventListener("submit", function(event) {
+        // Detener el comportamiento predeterminado del formulario
+        event.preventDefault();
+        
+        // Verificar la existencia de contenido válido en los elementos del formulario
+        var nombre = document.getElementById("nombre").value.trim();
+        var email = document.getElementById("email").value.trim();
+        
+        
+        // Validar el nombre
+        if (nombre === "") {
+            window.alert("Por favor, introduzca su nombre");
+        } 
+        
+        // Validar el email
+        if (email === "") {
+          window.alert("Por favor, introduzca su email");
+        } 
+    });
   
     window.alert("landing page loaded");
     console.log( eventLoaded );
@@ -10,3 +30,27 @@ let loaded = ( eventLoaded ) => {
   
   }
   window.addEventListener("DOMContentLoaded", loaded);
+
+
+const formulario = document.getElementById('formulario');
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const nombre = document.getElementById('nombre').value;
+    const email = document.getElementById('email').value;
+    const datos = {
+    nombre: nombre,
+    email: email
+    };
+    fetch('https://realtime-database-3e579-default-rtdb.firebaseio.com/collection.json', {
+        method: 'POST',
+        body: JSON.stringify(datos),
+        headers: {
+        'Content-Type': 'application/json'
+        }
+    })
+    .then(respuesta => respuesta.json())
+    .then(datos => {
+        console.log(datos); 
+    })
+    .catch(error => console.error(error));
+});
