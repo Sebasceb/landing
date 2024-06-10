2560a57bcd589ce8452c16b3e7c352e7fa3361b0
your name <mcpoveda@espol.edu.ec>
2024-06-10 10:56:24 -0500

    Guia 09
    jdnmkldfngsdfg

--- js/main.js
+++ js/main.js
@@ -1,56 +1,84 @@
-let loaded = ( eventLoaded ) => {
+async function obtenerDatos() {
+    const url = 'https://realtime-database-3e579-default-rtdb.firebaseio.com/collection.json';
+    const respuesta = await fetch(url);
+    if (!respuesta.ok) {
+        console.error("Error:", respuesta.status);
+        return;
+    }
+    const datosJSON = await respuesta.json();
+    const mapaClanNombre = new Map();
+    for (const key in datosJSON) {
+        const { clan, nombre } = datosJSON[key];
+        if (mapaClanNombre.has(clan)) {
+            mapaClanNombre.get(clan).push(nombre);
+        } else {
+            mapaClanNombre.set(clan, [nombre]);
+        }
+    }
+    const cuerpoTabla = document.getElementById('tablebody');
 
-    document.getElementById("formulario").addEventListener("submit", function(event) {
-        // Detener el comportamiento predeterminado del formulario
+    cuerpoTabla.innerHTML = '';
+    mapaClanNombre.forEach((nombres, clan) => {
+        const fila = cuerpoTabla.insertRow();
+        fila.insertCell().textContent = clan;
+        fila.insertCell().textContent = nombres.length;
+    });
+}
+
+
+let loaded = (eventLoaded) => {
+    event.preventDefault();
+    const formulario = document.getElementById('formulario');
+    formulario.addEventListener('submit', (event) => {
         event.preventDefault();
-        
-        // Verificar la existencia de contenido válido en los elementos del formulario
-        var nombre = document.getElementById("nombre").value.trim();
-        var email = document.getElementById("email").value.trim();
-        
-        
-        // Validar el nombre
+
+        var nombre = document.getElementById("nombre").value.trim().toUpperCase();
+        var clan = document.getElementById("clan").value.trim().toUpperCase();
+        var email = document.getElementById("email").value.trim().toUpperCase();
+
         if (nombre === "") {
             window.alert("Por favor, introduzca su nombre");
-        } 
-        
-        // Validar el email
-        if (email === "") {
-          window.alert("Por favor, introduzca su email");
-        } 
-    });
-  
-    window.alert("landing page loaded");
-    console.log( eventLoaded );
-    debugger;
-    let myform = document.getElementById('formulario');
-    myform.addEventListener('submit', ( eventSubmit ) => { 
-        debugger;
-    })
-  
-  }
-  window.addEventListener("DOMContentLoaded", loaded);
-
-
-const formulario = document.getElementById('formulario');
-formulario.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const nombre = document.getElementById('nombre').value;
-    const email = document.getElementById('email').value;
-    const datos = {
-    nombre: nombre,
-    email: email
-    };
-    fetch('https://realtime-database-3e579-default-rtdb.firebaseio.com/collection.json', {
-        method: 'POST',
-        body: JSON.stringify(datos),
-        headers: {
-        'Content-Type': 'application/json'
+            formulario.focus()
+            return;
         }
-    })
-    .then(respuesta => respuesta.json())
-    .then(datos => {
-        console.log(datos); 
-    })
-    .catch(error => console.error(error));
-});
+
+        if (email === "") {
+            window.alert("Por favor, introduzca su clan");
+            formulario.focus()
+            return;
+        }
+
+        if (email === "") {
+            window.alert("Por favor, introduzca su email");
+            formulario.focus()
+            return;
+        }
+
+        const datos = {
+            nombre: nombre,
+            clan: clan,
+            email: email
+        };
+        fetch('https://realtime-database-3e579-default-rtdb.firebaseio.com/collection.json', {
+            method: 'POST',
+            body: JSON.stringify(datos),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(respuesta => respuesta.json())
+            .then(datos => {
+                alert("¡Tu respuesta ha sida guardada!")
+                obtenerDatos()
+            })
+            .catch(error => console.error(error));
+    });
+
+    obtenerDatos()
+}
+
+
+window.addEventListener("DOMContentLoaded", loaded);
+
+
+
